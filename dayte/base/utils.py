b466fec7e444993f4e976fed0d7cc79c1db50e19
bechir hamdi <hbechir52@gmail.com>
2023-10-18 17:26:17 +0100

    Update utils.py

--- dayte/base/utils.py
+++ dayte/base/utils.py
@@ -4,13 +4,24 @@ from .models import Profile
 import random
 
 def getSuggestions(user, profile):
+    """gets a list of profiles to display on the home page"""
+
     # Get all profiles that have at least one interest in common with the given profile
     common_profiles = Profile.objects.filter(interests__in=profile.interests.all()).exclude(user=user)
-    
 
-    #number of common interests between the two profiles 
-
-
+    grid = userPlanGrid(user)
     # Shuffle the profiles and return a list of 9 users
-    opposite_gender_profiles = random.sample(list(common_profiles), min(len(common_profiles), 9))
+    opposite_gender_profiles = random.sample(list(common_profiles), min(len(common_profiles), grid))
     return User.objects.filter(profile__in=opposite_gender_profiles)
+
+
+def userPlanGrid(user):
+    """ returns the number of profiles to display daily based on the user's plan """
+    if user.profile.plan == 'free':
+        return 2
+    elif user.profile.plan == 'basic':
+        return 9
+    elif user.profile.plan == 'premium':
+        return 12
+    else:
+        return 0
